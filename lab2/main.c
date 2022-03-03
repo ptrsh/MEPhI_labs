3e152b47d2fd63a1b5eabbdeea43e9919ba25f55
Kirill Petrushin <chirik@laptop.localdomain>
2022-03-03 20:22:06 +0300

    readline was deleted

--- lab2/main.c
+++ lab2/main.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
 #include "library/stack.h"
-#include <readline/readline.h>
 #include <malloc.h>
+#include <string.h>
 
-
+char *get_str();
 int priority(char ch);
 int is_var(char ch);
 void infix_to_postfix(char *str);
 
 int main(){
-    printf("Введите инфиксную нотацию\n");
-    char *input = readline("[Input] ");
+    printf("Введите инфиксную нотацию:\n");
+    char *input = get_str();
     infix_to_postfix(input);
     free(input);
     return 0;
+}
+
+char *get_str() {
+    char buf[81] = {0};
+    char *res = NULL;
+    int len = 0;
+    int n = 0;
+    do {
+        n = scanf("%80[^\n]", buf);
+        if (n < 0) {
+            if (!res) {
+                return NULL;
+        }
+    } else if (n > 0){
+        int chunk_len = strlen(buf);
+        int str_len = len + chunk_len;
+        res = realloc(res, str_len + 1);
+        memcpy(res + len, buf, chunk_len);
+        len = str_len;
+    } else
+        scanf("%*c");
+    
+    } while (n>0);
+    if (len > 0)
+        res[len] = '\0';
+    else 
+        res = calloc(1, sizeof(char));
+    return res;
 }
 
 int priority(char ch) {
