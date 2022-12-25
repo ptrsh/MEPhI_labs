b47817d1ab20bb54d0448582b3247dbda39b6cfb
Kirill Petrushin <chirik@laptop.localdomain>
2022-12-25 13:33:21 +0300

    add Doxygen

--- OOP/lab4/main.cpp
+++ OOP/lab4/main.cpp
@@ -1,6 +1,15 @@
 
 #include "lib/app.h"
 
+/**
+ * @brief Entry point
+ *
+ * Execution of the program
+ * starts here.
+ *
+ * @return Program exit status
+ */
+
 int main () {
     App app;
     app.start();
